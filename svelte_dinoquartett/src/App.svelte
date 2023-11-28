af13ef28f6201f6362343457a260603465d205ed
FR <mail@rauch-fabian.de>
2023-11-28 14:45:36 +0100

    231128-mid_course

--- svelte_dinoquartett/src/App.svelte
+++ svelte_dinoquartett/src/App.svelte
@@ -1,13 +1,23 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
 </script>
 
-<main>
-  <div>
-    I am a svelte developer!
-</main>
+<div id="wrapper">
+  <header>
+      <h1>Beste Dino-Quartett</h1>
+  </header>
+  <nav>
+      <a href="index.html">Startseite</a>
+      <a href="spiel.html">Spiel</a>
+      <a class="only-desktop" href="highscore.html">Highscore</a>
+  </nav>
+  <main>
+      <div id="cards-container"></div>
+  </main>
+  <footer>
+      <a href="impressum.html">Impressum</a>
+      <a href="datenschutz.html">Datenschutz</a>
+  </footer>
+</div>
 
 <style>
  
